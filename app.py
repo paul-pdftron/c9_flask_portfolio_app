794937a411491db81092f7cd5986ed8a9d5b01bd
Marc <bmarc4321@gmail.com>
2019-10-10 21:12:13 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,99 +1,54 @@
-from flask import Flask, render_template, request, redirect
-import datetime
-import pytz # timezone 
-import requests
-import os
-
+from flask import Flask, render_template, redirect, url_for
 
 
 app = Flask(__name__)
 
+@app.route("/")
+def home():
+  return render_template("index.html")
 
-@app.route('/', methods=['GET'])
-def home_page():
-	return render_template('index.html')
+@app.route("/login")
+def login():
+    return render_template("login.html")
 
-@app.route('/<name>')
-def profile(name):
-	return render_template('index.html', name=name)
+@app.route("/forgotpassword")
+def forgotpassword():
+    return render_template("forgot-password.html")
 
+@app.route("/register")
+def register():
+    return render_template("register.html")
 
-@app.route('/add_numbers', methods=['GET','POST'])
-def add_numbers_post():
-	  # --> ['5', '6', '8']
-	  # print(type(request.form['text']))
-	  if request.method == 'GET':
-	  	return render_template('add_numbers.html')
-	  elif request.method == 'POST':
-  	      print(request.form['text'].split())
-  	      total = 0
-  	      try:
-  	      	for str_num in request.form['text'].split():
-  	      		total += int(str_num)
-  	      	return render_template('add_numbers.html', result=str(total))
-  	      except ValueError:
-  	      	return "Easy now! Let's keep it simple! 2 numbers with a space between them please"
+@app.route("/tryitnowpg")
+def tryitnowpg():
+    return render_template("tryitnow.html")
 
+@app.route('/whatiscodercats')
+def whatiscodercats():
+    return redirect(url_for('home',_anchor='whatiscodercats'))
 
-@app.route('/shopping_list', methods=['GET','POST'])
-def shopping_list_post():
-	  # --> ['5', '6', '8']
-	  # print(type(request.form['text']))
+@app.route('/howitworks')
+def howitworks():
+    return redirect(url_for('home',_anchor='howitworks'))
 
-    if request.method == 'GET':
-      return render_template('shopping_list.html')
-    elif request.method == 'POST':
-          print(request.form['text'].split())
-          
-          shop_list = []
-          try:
-            for item in request.form['text'].split():
-              
-              shop_list.append(item)
-
-              
-              
-            return render_template('shopping_list.html', result="\n".join([str(item) for item in shop_list]))
-          except ValueError:
-            return "Easy now! Let's keep it simple! Just words with a space between them"
-          
-  	      
-@app.route('/time', methods=['GET','POST'])
-def time_post():
-    # --> ['5', '6', '8']
-    # print(type(request.form['text']))
-
-    if request.method == 'GET':
-      return render_template('time.html')
-    elif request.method == 'POST':
-          print(request.form['text'].split())
-          
-          for item in request.form['text'].split():
-            answer = (datetime.datetime.now(pytz.timezone("Europe/Dublin")).strftime('Time = ' + '%H:%M:%S' + ' GMT ' + ' Year = ' + '%d-%m-%Y'))
-            #answer = datetime.datetime.now().strftime('Time == ' + '%H:%M:%S' + ' Year == ' + '%d-%m-%Y')
-            #answer = datetime.datetime.now().strftime('%Y-%m-%d \n %H:%M:%S')
-
-              
-              
-            return render_template('time.html', result=answer)
-
-         
-
-@app.route('/python_apps')
-def python_apps_page():
-	# testing stuff
-	return render_template('python_apps.html')
-
+@app.route('/tryitnow')
+def tryitnow():
+    return redirect(url_for('home',_anchor='tryitnow'))
 
 @app.route('/contact')
 def contact():
-	return render_template('contact.html')
+    return redirect(url_for('home',_anchor='contact'))
 
-@app.route('/blog', methods=['GET'])
-def blog_page():
-  return render_template('blog.html')
 
-app.run(host=os.getenv('IP', '0.0.0.0'), port = int(os.getenv('PORT', 8080)))
+@app.route("/dashboard")
+def dashboard():
+    return render_template("dashboard.html")
 
-if __name__ == '__main__':
-	app.run(debug=False)
+
+
+from datetime import datetime
+@app.context_processor
+def inject_now():
+    return {'now': datetime.utcnow()}
+
+app.run(debug=True)
