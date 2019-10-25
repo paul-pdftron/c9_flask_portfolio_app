0cd7cc599d8bb9e1f84a23dd2fe6b5da0540efb4
Paul Laoha <plaoha@pdftron.com>
2019-10-24 21:01:27 -0700

    fix webviewer code

--- static/js/login_register_flipper.js
+++ static/js/login_register_flipper.js
@@ -1,10 +1,14 @@
 var loginButton = document.getElementById("loginButton");
 var registerButton = document.getElementById("registerButton");
 
-loginButton.onclick = function(){
-	document.querySelector("#flipper").classList.toggle("flip");
+if (!!loginButton) {
+	loginButton.onclick = function(){
+		document.querySelector("#flipper").classList.toggle("flip");
+	}
 }
 
-registerButton.onclick = function(){
-	document.querySelector("#flipper").classList.toggle("flip");
+if (!!registerButton) {
+	registerButton.onclick = function(){
+		document.querySelector("#flipper").classList.toggle("flip");
+	}
 }
