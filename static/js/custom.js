ea8d1132b3bd51609dc12e532795d43fd8ba4ca2
Marc <bmarc4321@gmail.com>
2019-10-27 11:40:26 +0000

    Edit config file

--- static/js/custom.js
+++ static/js/custom.js
@@ -1,18 +1,3 @@
-$(document).on('documentLoaded', function() {
-  var doc = docViewer.getDocument();
-  doc.loadThumbnailAsync(0, function(thumb) {
-    $('#myThumbnailDiv').append(thumb);
-  });
-
-  var annotManager = docViewer.getAnnotationManager();
-  var rectangle = new Annotations.RectangleAnnotation();
-  rectangle.PageNumber = 2;
-  rectangle.X = 100;
-  rectangle.Y = 100;
-  rectangle.Width = 250;
-  rectangle.Height = 250;
-  rectangle.Author = annotManager.getCurrentUser();
-  annotManager.addAnnotation(rectangle);
-
-  docViewer.displayLastPage();
-});
+$(document).addEventListener('DOMContentLoaded', function() {
+      var ann = new Annotator(document.body)
+  }, false);
