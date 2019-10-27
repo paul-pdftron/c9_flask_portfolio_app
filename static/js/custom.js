fc830cb535ac16051eef181004322e877170fafe
Marc <bmarc4321@gmail.com>
2019-10-27 11:46:16 +0000

    Webviewer config edit

--- static/js/custom.js
+++ static/js/custom.js
@@ -1,6 +1,7 @@
-$(document).on('viewerLoaded', function() {
-  docViewer.setMargin(20);
-  docViewer.on('fitModeUpdated', function(e, fitMode) {
-    console.log('fit mode changed');
-  });
-});
+WebViewer(...)
+  .then(function(instance) {
+    input.addEventListener('change', function() {
+      // Get the file from the input
+      var file = input.files[0];
+      instance.loadDocument(file, { filename: file.name });
+    });
